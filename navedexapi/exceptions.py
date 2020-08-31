f12b4f1041831cab146ffed252e3a75d926272c0
Germanno Correia <germanno.correia@simbioseventures.com>
2020-08-31 09:50:28 -0300

    Add all exceptions related to invalid and unexistent fields

--- navedexapi/exceptions.py
+++ navedexapi/exceptions.py
@@ -12,3 +12,27 @@ class InvalidFieldType(APIException):
     status_code = 400
     default_detail = "Invalid or incorrect type of one of the fields in the request. All fields are of type string."
     default_code = "incorrect_field_type"
+
+
+class InvalidQueryParam(APIException):
+    status_code = 400
+    default_detail = "Invalid or incorrect query param in the request."
+    default_code = "incorrect_query_param"
+
+
+class InvalidIdentifier(APIException):
+    status_code = 400
+    default_detail = "Invalid or incorrect object unique identifier in the request."
+    default_code = "incorrect_identifier"
+
+
+class NaverNotFound(APIException):
+    status_code = 404
+    default_detail = "Naver object not found."
+    default_code = "naver_not_found"
+
+
+class ProjectNotFound(APIException):
+    status_code = 404
+    default_detail = "Project object not found."
+    default_code = "project_not_found"
