64a87e04defe642dc898bacb3fcd9218b62a1117
Germanno Correia <germanno.correia@simbioseventures.com>
2020-09-09 10:14:27 -0300

    Correct project name

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'navedexconfig.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emprestimo_api_config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
